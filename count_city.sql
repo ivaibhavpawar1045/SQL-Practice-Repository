064f3e93b515ba47312867c921e1de2b3c39a873
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-07-27 20:56:24 +0530

    Solved HackerRank SQL : Count total population

---
+++ count_city.sql
@@ -0,0 +1,12 @@
+Problem :-
+               Query the total population of all cities in CITY where District is California.
+Platform :-
+               HackerRank
+               
+Difficulty :- 
+               Easy
+
+
+SELECT sum(POPULATION)
+FROM CITY 
+where DISTRICT = "California"
