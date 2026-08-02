a12c45355884ec427d9c629b0a8808e1e236e351
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-08-02 19:58:57 +0530

    Solved LeetCode SQL : Employee earn more than Manager

--- employee_salary_more.sql
+++ employee_salary_more.sql
@@ -1,7 +1,7 @@
 Problem :-
                Write a solution to find the employees who earn more than their managers.
                Return the result table in any order.
-               
+
 Platform :-
                LeetCode
                
