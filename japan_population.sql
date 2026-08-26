40175ec281b7efd33d3e246b8a6da99057432ce2
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-08-26 21:52:52 +0530

    Solved HackerRank SQL : Sum of Population in all cities of Japan

---
+++ japan_population.sql
@@ -0,0 +1,14 @@
+
+Problem :-
+              write a query for sum of populations for all cities in Japan
+Platform :-
+               HackerRank
+               
+Difficulty :- 
+               Mid
+
+
+
+SELECT POPULATION
+FROM CITY
+WHERE COUNTRYCODE = "JPN";
