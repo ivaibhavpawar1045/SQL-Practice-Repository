bfcba5cd421abe25792cf647c9ab5c263567720a
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-08-28 19:17:35 +0530

    Solved HackerRank SQL : City having population larger than 100,000

---
+++ count_population.sql
@@ -0,0 +1,11 @@
+Problem :-
+             Query a count of the number of cities in CITY having a Population larger than 100,000.
+Platform :-
+               HackerRank
+               
+Difficulty :- 
+               Mid
+
+SELECT COUNT(NAME)
+FROM CITY
+WHERE POPULATION > 100000
