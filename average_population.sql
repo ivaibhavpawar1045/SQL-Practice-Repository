471e831e06267fbf0c49df0a920bbadc537a161a
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-09-01 23:04:02 +0530

    Solved HackerRank SQL : Average of Population

---
+++ average_population.sql
@@ -0,0 +1,13 @@
+Problem :-
+             Query the average population of all cities in CITY where District is California.
+
+Platform :-
+               HackerRank
+               
+Difficulty :- 
+               Easy
+
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = "California"
