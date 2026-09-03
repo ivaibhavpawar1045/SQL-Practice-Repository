73609fb613aa1ba603ea08c49f507c27ece5b447
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-09-03 23:02:42 +0530

    Solved HackerRank SQL : Calculate Average of Population with Rounded one

---
+++ avg_round_population.sql
@@ -0,0 +1,12 @@
+Problem :-
+             Query the average population for all cities in CITY, rounded down to the nearest integer.
+Platform :-
+               HackerRank
+
+Difficulty :- 
+               Mid
+
+
+
+SELECT FLOOR(AVG(POPULATION))
+FROM CITY;
